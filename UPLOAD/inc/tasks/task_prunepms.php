f588b895835de5da772e8dbbcbb52a7b614489d2
Cu8e Cu8eeeR <Eldenroot@gmail.com>
2014-11-23 20:07:33 +0100

    v0.7.1

--- UPLOAD/inc/tasks/task_prunepms.php
+++ UPLOAD/inc/tasks/task_prunepms.php
@@ -12,28 +12,20 @@ function task_pmprune($task)
     global $db;
 
     // PMs older than x seconds will be deleted
-    $secs = 300; // 30 days
-    $usecs = 900; // 60 days
+    $secs = 30*24*3600; // 30 days
+    $usecs = 60*24*3600; // 60 days
     // Do not delete unread PMs
-    $db->delete_query("privatemessages", "(dateline<".(TIME_NOW-$secs)." AND readtime>0) OR dateline<".(TIME_NOW-$usecs));
+    $db->delete_query("privatemessages", "(dateline<".(TIME_NOW-$secs)." AND readtime>0) OR dateline<".(TIME_NOW-$usecs)." AND (folder='1' OR folder='2' OR folder='3' OR folder='4'));
 	// Recount PMs after cleaning
     $queryString  = <<<SQL
     UPDATE mybb_users u SET 
         totalpms = (SELECT COUNT(pmid) FROM mybb_privatemessages pm WHERE pm.uid = u.uid),
         unreadpms = (SELECT COUNT(pmid) FROM mybb_privatemessages pm WHERE
-            pm.uid = u.uid AND status='0' AND folder='1');
+            pm.uid = u.uid AND status='0' AND (folder='1' OR folder='2' OR folder='3' OR folder='4'));
 SQL;
 
     $db->write_query($queryString);
-}
-
-// Optimize DB
-function task_optimizedb($task)
-{
-	global $db;
-	
-	$db->query("OPTIMIZE TABLE `".TABLE_PREFIX."_privatemessages`, `".TABLE_PREFIX."_tasklog`, `".TABLE_PREFIX."_tasks`");
 	
 	// Add a log
-	add_task_log($task, "The database optimization task after PMs cleaning successfully ran.");
+	add_task_log($task, The Prune old PMs task successfully ran.);
 }
